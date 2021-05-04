57965a97c9b471d558f8826ff8e9a888d1c9ba5d
earl706 <79135732+earl706@users.noreply.github.com>
2021-05-04 17:32:58 +0800

    Update Create Phone Number.cpp

--- Create Phone Number.cpp
+++ Create Phone Number.cpp
@@ -31,8 +31,6 @@ std::string createPhoneNumber(const int arr [10]){
 
 //better solution
 
-#include <string>
-
 std::string createPhoneNumber(const int arr [10]){
   char buf[15];
   snprintf(buf, sizeof(buf), "(%d%d%d) %d%d%d-%d%d%d%d%d", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9]);
